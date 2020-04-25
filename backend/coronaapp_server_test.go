11f0a08d2a2e39f3705ad532a2c5277d6254b2ac
Sebastian Roy <seb.roy@t-online.de>
2020-04-25 18:51:42 +0200

    Minor changes

--- backend/coronaapp_server_test.go
+++ backend/coronaapp_server_test.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"testing"
 	"fmt"
+	"testing"
+
 	//gonfig "github.com/eduardbcom/gonfig"
 	"database/sql"
-   // "database/sql/driver"
-    _ "encoding/json"
+	// "database/sql/driver"
+	_ "encoding/json"
 )
 
 func init() {
 	dbconf := &DBConfig{
-		Host: "db-postgresql-fra1-47535-do-user-1884949-0.a.db.ondigitalocean.com",
-		Port: 25060,
-		User: "doadmin",
-		Password: "y1m27iy7ahk9t0a4",
-		DBName: "defaultdb",
-		SSL: "require",
+		Host:     "db-postgresql-fra1-47535-do-user-1884949-0.a.db.ondigitalocean.com",
+		Port:     25060,
+		User:     "doadmin",
+		Password: "",
+		DBName:   "defaultdb",
+		SSL:      "require",
 	}
 
-    var err error
+	var err error
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=%s",
 		dbconf.Host, dbconf.Port, dbconf.User, dbconf.Password, dbconf.DBName, dbconf.SSL)
-    db, err = sql.Open("postgres", psqlInfo)
+	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
@@ -37,9 +38,9 @@ func init() {
 }
 func TestDBGetPushtoken(t *testing.T) {
 	token := DBGetPushtoken("rBpyst6rNbTGn7ltJwtDgjrbU2G3")
-	var testtoken pushtoken 
+	var testtoken pushtoken
 
-	testtoken.FirebasePushtoken  = "fPpMG6fxW8E:APA91bGi-KDyZ1jZ-I_6piBicdrbxtM6amaruWMIxWgf42AZB0R4AzbeM2fguXp4auGhmYIh9EXOK6UYd-dHolqb1mHH6HslnuK14uLVFaBWF_CHzJLP6HOdDrmayVh8Hc9Zbnk3Lv-T" 
+	testtoken.FirebasePushtoken = "fPpMG6fxW8E:APA91bGi-KDyZ1jZ-I_6piBicdrbxtM6amaruWMIxWgf42AZB0R4AzbeM2fguXp4auGhmYIh9EXOK6UYd-dHolqb1mHH6HslnuK14uLVFaBWF_CHzJLP6HOdDrmayVh8Hc9Zbnk3Lv-T"
 	t.Log(token)
 	if token != testtoken {
 		t.Errorf("Token was incorrect, got %v, want: %v", token.FirebasePushtoken, testtoken.FirebasePushtoken)
