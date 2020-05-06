db3f49e06ae50756d42e2a995d80544a84950480
Sebastian Roy <seb.roy@t-online.de>
2020-05-06 19:53:44 +0200

    Details to add friends modal view

--- frontend/Corozone/app/components/ModalAddFriends.vue
+++ frontend/Corozone/app/components/ModalAddFriends.vue
@@ -18,7 +18,7 @@
     
 		<Button class="btn btn-outline" text="Open Camera" @tap="$modal.close()" v-if="this.$isAndroid"/>
         <Image :src="friendQRCodeURL" height="200"></Image>
-		<Label class="h4 text-center" :text="this.generateCode" textWrap="true"/>
+		<Label class="h4 text-center" :text="this.generateCode" textWrap="true" v-if="addUserIDByHand"/>
 		<!-- <Label class="h2 text-center" :text="me.firstName"/> -->
 
         <!-- <TextField
@@ -37,11 +37,12 @@
               hint="Paste code to add friend"
               editable="false"
               @returnPress="onNameTap"
+              v-if="addUserIDByHand"
             />
             <Label class="h4 text-center" text="Why do we use QR Codes? We don't want other people have access to your personalized information
             without your permission and don't use our severs if we don't have to. We constantly balance privacy, security and convenience so this may change in the future." textWrap="true"/>
-             <Button class="btn btn-outline" text="Add friend" @tap="" />
-		<Button class="btn btn-outline" text="Close Modal" @tap="$modal.close()" />
+             <Button class="btn btn-outline" text="Add friend" @tap="" v-if="addUserIDByHand" />
+		<Button class="btn btn-outline" text="Close" @tap="$modal.close()" />
 	</StackLayout>
 </template>
 
@@ -51,7 +52,8 @@ import { mapState, mapGetters } from "vuex";
 export default {
     data() {
         return {
-            friendQRCodeURL: ""
+            friendQRCodeURL: "",
+            addUserIDByHand: false
         };
     },
     created(){
@@ -71,14 +73,28 @@ export default {
     },
     computed: {
          ...mapState(["me"]),
+         // TODO: Add Hash to check data integrity
          generateCode: function () {return this.$backendService.token+":"+this.me.firstName  }
     },
     methods: {
         getFriendQRCode() {
             return "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=app://corozone/"+this.generateCode
         },
-        onScanResult(args) {
-            console.log(args)
+        onScanResult(scanEvent) {
+            console.log(`Scan Result: ${scanEvent.text} (${scanEvent.format})`)
+            const friend = {
+                uid: scanEvent.text.split("corozone/")[1].split(":")[0],
+                name: scanEvent.text.split("corozone/")[1].split(":")[1]
+            }
+            if(this.$store.getters.getFriendListIDs.some(e => e.uid === friend.uid)) {
+      this.$store.commit("setNotificationMessage", "Friend already exists") 
+    }
+    else {
+    this.$store.commit("addFriendID", friend)
+      this.$store.commit("setNotificationMessage", "Friend " + friend.name + " added")
+      $modal.close() 
+    }
+           
         },
         // generateCode() {
         //    return this.$backendService.token+":"+this.me.firstName 
